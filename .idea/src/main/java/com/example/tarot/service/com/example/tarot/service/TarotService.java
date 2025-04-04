466aed93264aa5570799173d7d92bd057b2a5e4e
Aishani De Sirkar <aishani.ds@gmail.com>
2025-04-04 16:53:16 -0500

    Getting started

--- .idea/src/main/java/com/example/tarot/service/com/example/tarot/service/TarotService.java
+++ .idea/src/main/java/com/example/tarot/service/com/example/tarot/service/TarotService.java
@@ -1,4 +1,23 @@
 package main.java.com.example.tarot.service.com.example.tarot.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TarotService {
+
+    public List<String> shuffleDeck() {
+        //Step 1: Create the deck
+        List<String> deck = new ArrayList<>();
+        for (int i = 1; i <= 78; i++) {
+            deck.add("Card " + i);
+        }
+        return deck;
+    }
+
+    Collections.shuffle(deck);
+
+
+    System.out.println("Please choose what spread you would like: /nSingle Card /nThree-Spread" +
+            "/nCeltic Cross");
 }
